1102cc5dd7f4279bde665b462c5260caa95dd9fd
Natasha <natasha.kusuma@outlook.com>
2022-07-08 14:41:12 +1000

    done

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
   //add static methods here
+  static capitalize(word){
+    let wordSpread  = word.split('');
+    wordSpread[0] = wordSpread[0].toUpperCase();
+    return wordSpread.join('');
+  }
+
+  static sanitize(word){
+  var textArr = word.replace(/[*+!@#[$%%^&:"\|/{_}();<>,.?\r\n]/g, '');
+  return textArr;
+
+  }
+
+  static titleize(sentence){
+    let words = sentence.split(" ");
+    words[0] = this.capitalize(words[0]);
+    for (let i = 1; i<words.length; i++){
+      if (words[i] === "the"||words[i] =="a"||words[i] ==="an" ||words[i]==="but" || words [i] === "of" || words[i]
+      ==="and" || words[i] === "for" || words[i] ==="at" ||words[i] ==="by" || words[i] === "from") {
+      words[i] = words[i]
+      } else {
+        words[i]= this.capitalize(words[i]);
+      }
+  }
+
+  return words.join(" ");
+}
 }
